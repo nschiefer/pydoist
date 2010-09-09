fffe17dd840715f7af790a45856b4bc935ec664e
Nicholas Schiefer <nicholasschiefer@gmail.com>
2010-09-08 21:34:24 -0400

    Replaced find_by_name with general find function with variable argument lists.

--- project.py
+++ project.py
@@ -16,9 +16,25 @@ class Project:
     def delete(self):
         connect.connect(method="DELETE", url="deleteProject", params={'token': self.user.api_token, 'project_id': self.id})
 
+    def update(self, name=None, color=None, indent=None):
+        params = {'token': self.user.api_token, 'project_id': self.id}
+
+        if(name):
+            params['name'] = name
+            self.name = name
+        if(color):
+            params['color'] = color
+            self.color = color
+        if(indent):
+            params['indent'] = indent
+            self.indent = indent
+
+        connect.connect(method="PUT", url="updateProject", params=params)
+
 class ProjectList(list):
     def add(self, name, color=None, indent=None, order=None):
         params = {'token': self[0].user.api_token, 'name': name}
+
         if(color):
             params['color'] = color
         if(indent):
@@ -29,11 +45,18 @@ class ProjectList(list):
         json_data = connect.connect(method="POST", url="addProject", params=params)
         return Project(json_data, self[0].user.api_token)
 
-    def find_by_name(self, name):
+    def find(**kwargs):
         matches = ProjectList()
-        for project in self:
-            if project.name == name:
-                matches.append(project)
+        if kwargs.pop('id', None):
+            for project in self:
+                if project.id == kwargs['id']:
+                    matches.append(project)
+        elif kwargs.pop('name', None):
+            for project in self:
+                if project.name == kwargs['name']:
+                    matches.append(project)
+        else:
+            matches = self
 
         if len(matches) == 0:
             return None
@@ -41,4 +64,14 @@ class ProjectList(list):
             return matches[0]
         else:
             return matches
+
+    def delete(self):
+        for project in self:
+            project.delete()
+
+        self = ProjectList()
+
+    def update(self, name=None, color=None, indent=None):
+        for project in self:
+            project.update(name, color, indent)
 
