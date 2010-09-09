399d9195dd841c2d34ad4a4f61cb424df8c08e77
Nicholas Schiefer <nicholasschiefer@gmail.com>
2010-09-08 21:07:42 -0400

    Implemented delete method as part of project. Added ProjectList class, an extended list with add and find methods (ala Rails).

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import connect
 
 class Project:
-    def __init__(self, json_data):
+    def __init__(self, json_data, user):
         self.user_id = json_data['user_id']
         self.name = json_data['name']
         self.color = json_data['color']
@@ -10,4 +10,35 @@ class Project:
         self.indent = json_data['indent']
         self.cache_count = json_data['cache_count']
         self.id = json_data['id']
+
+        self.user = user
+
+    def delete(self):
+        connect.connect(method="DELETE", url="deleteProject", params={'token': self.user.api_token, 'project_id': self.id})
+
+class ProjectList(list):
+    def add(self, name, color=None, indent=None, order=None):
+        params = {'token': self[0].user.api_token, 'name': name}
+        if(color):
+            params['color'] = color
+        if(indent):
+            params['indent'] = indent
+        if(order):
+            params['order'] = order
+
+        json_data = connect.connect(method="POST", url="addProject", params=params)
+        return Project(json_data, self[0].user.api_token)
+
+    def find_by_name(self, name):
+        matches = ProjectList()
+        for project in self:
+            if project.name == name:
+                matches.append(project)
+
+        if len(matches) == 0:
+            return None
+        elif len(matches) == 1:
+            return matches[0]
+        else:
+            return matches
 
