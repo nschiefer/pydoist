07c34d5c4bf9d83d3624479b14fdee3cdce14310
Nicholas Schiefer <nicholasschiefer@gmail.com>
2010-10-22 22:44:02 -0400

    Minor tweaks to item data storage interface.

--- item.py
+++ item.py
@@ -4,13 +4,13 @@ class Item:
     def __init__(self, json_data, user):
         self.due_date = json_data['due_date']
         self.user_id = json_data['user_id']
-        self.in_history = json_data['in_history']
+        self.in_history = not(not(json_data['in_history']))
         self.priority = json_data['priority']
         self.item_order = json_data['item_order']
         self.content = json_data['content']
         self.indent = json_data['indent']
         self.project_id = json_data['project_id']
-        self.checked = json_data['checked']
+        self.checked = not(not(json_data['checked']))
         self.date_string = json_data['date_string']
         self.id = json_data['id']
 
